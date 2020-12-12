967ff6f02e7f32b5dfb6cb266ab702498cff1a69
tbonner1 <tennessee.bonner@hcl.com>
2020-12-12 13:03:52 -0600

    Sprint 1: Creating welcome screen.

---
+++ LockedMeMain.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class LockedMeMain
+{
+	public static void main(String[] args)
+	{
+		//The following print statements represent what we should expect for the welcome menu to start
+		System.out.println("\n==========================================\n");
+		System.out.println("        LOCKEDME.COM  FILE MANAGER");
+		System.out.println("\n==========================================\n");
+		System.out.println("WELCOME! PLEASE MAKE YOUR CHOICE BELOW:\n");
+		
+		System.out.println("1. View Files in ascending order\n");
+		System.out.println("2. Modify files\n");
+		System.out.println("3. Close Application\n");
+	}
+}
